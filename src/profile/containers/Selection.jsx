a3ba46e95e6e99ac6dae006f4c56997b6cc8a0dc
Roger Rengifo <thelea12@gmail.com>
2021-05-13 10:10:37 -0500

    delay modal

--- src/profile/containers/Selection.jsx
+++ src/profile/containers/Selection.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectProfileInit, openModal } from '../../store/actions';
 import { AlertTriangle } from 'react-feather';
@@ -15,13 +15,13 @@ const Selection = () => {
   const dispatch = useDispatch();
   const { isLoading, profiles } = useSelector((state) => state.Profile);
 
-  // useEffect(() => {
-  //   const timeout = setTimeout(() => {
-  //     setModalType('info');
-  //     dispatch(openModal());
-  //   }, 600);
-  //   return () => clearTimeout(timeout);
-  // }, [dispatch]);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setModalType('info');
+      dispatch(openModal());
+    }, 600);
+    return () => clearTimeout(timeout);
+  }, [dispatch]);
 
   const addProfileHandler = () => {
     setModalType('profile');
@@ -63,8 +63,8 @@ export const InfoModal = () => (
     <AlertTriangle size={70} className='error-msg mb-4' />
     <h2>Estimado usuario</h2>
     <p>
-      Le informamos que en estos momentos la plataforma para empresas de <b>BCP</b> se encuentra colapsada. Por tal motivo, las operaciónes hacia y desde este banco no podrán ser
-      realizadas por el momento. Lo Hemos notificado y están trabajando para solucionarlo. <br />
+      Le informamos que en estos momentos la plataforma para empresas de <b>BCP</b> e <b>Interbank</b> se encuentran con retrasos de conexión. Por tal motivo, sus operaciones
+      podrán demorar más de lo usual. <br />
       <span className='mt-4 inline-block font-bold'>Agradecemos su comprensión.</span>
     </p>
   </div>
