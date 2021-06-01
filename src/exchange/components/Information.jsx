4a99e73c4d65597d541f4c7925c2ee1413b1f303
Roger Rengifo <thelea12@gmail.com>
2021-05-31 20:45:57 -0500

    fixing img in echange

--- src/exchange/components/Information.jsx
+++ src/exchange/components/Information.jsx
@@ -1,48 +1,50 @@
-import React from 'react';
-import { isMobile } from 'react-device-detect';
+import React from "react";
+import { isMobile } from "react-device-detect";
 
-import Card from '../../core/components/UI/Card';
-import Button from '../../core/components/UI/Button';
+import Card from "../../core/components/UI/Card";
+import Button from "../../core/components/UI/Button";
 
-import InfoImg from '../images/exchange-user.svg';
-import Clock from '../images/icons/clock.svg';
-import Arrows from '../images/icons/arrows.svg';
-import User from '../images/icons/user.svg';
-import Dollar from '../images/icons/dollar.svg';
+import InfoImg from "../images/exchange-user.png";
+import Clock from "../images/icons/clock.svg";
+import Arrows from "../images/icons/arrows.svg";
+import User from "../images/icons/user.svg";
+import Dollar from "../images/icons/dollar.svg";
 
-import classes from '../containers/Exchange.module.scss';
+import classes from "./Info.module.scss";
 
 const Information = ({ onClose }) => {
   return (
     <div className={classes.Information}>
-      <div className='flex items-end justify-center'>
-        <img src={InfoImg} alt='information' className={classes.InfoImg} width={120} />
-        <div className='flex items-end justify-center flex-wrap lg:flex-nowrap'>
-          <div className='w-full'>
-            <h3>¡Importante!</h3>
+      <div className="flex items-end justify-center">
+        <div className="flex items-end justify-center flex-wrap relative lg:flex-nowrap">
+          <h3 className={classes.InfoTitle}>¡Importante!</h3>
+          <Card className={classes.InfoImgCard}>
+            <img src={InfoImg} alt="information" />
+          </Card>
+          <div className="w-full">
             <Card className={classes.InfoCard}>
-              <img src={Arrows} alt='arrows' />
+              <img src={Arrows} alt="arrows" />
               <p>
                 Recibimos solo transferencias. <br /> No aceptamos depósitos.
               </p>
             </Card>
           </div>
           <Card className={classes.InfoCard}>
-            <img src={User} alt='clock' />
+            <img src={User} alt="clock" />
             <p>Ambas cuentas deben ser del titular.</p>
           </Card>
           <Card className={classes.InfoCard}>
-            <img src={Clock} alt='clock' />
+            <img src={Clock} alt="clock" />
             <p>Las transacciones se procesan entre 10 a 25 minutos.</p>
           </Card>
           <Card className={classes.InfoCard}>
-            <img src={Dollar} alt='clock' />
+            <img src={Dollar} alt="clock" />
             <p>Importes mayores de $10,000 pueden tardar más de lo usual.</p>
           </Card>
         </div>
       </div>
       {isMobile && (
-        <Button type='button' className='action-button' onClick={onClose}>
+        <Button type="button" className="action-button" onClick={onClose}>
           Aceptar
         </Button>
       )}
