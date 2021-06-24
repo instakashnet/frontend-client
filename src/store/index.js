39cab898318174e368d6ed79ac4d42fba0c85919
Roger Rengifo <thelea12@gmail.com>
2021-06-23 19:23:44 -0500

    removing redux devtools on prod

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,14 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducer";
 import createSaga from "redux-saga";
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import rootSaga from "./saga";
+
+const composeEnhancers = (process.env.NODE_ENV !== "production" && typeof window !== "undefined" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const initialState = {};
 const sagaMiddleware = createSaga();
 const middlewares = [sagaMiddleware];
-const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middlewares)));
+const store = createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(...middlewares)));
 
 sagaMiddleware.run(rootSaga);
 
