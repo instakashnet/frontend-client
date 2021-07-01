4ccfeaae0c31d31a43d9e3d3fa9139ca7b43931b
Roger Rengifo <thelea12@gmail.com>
2021-07-01 16:52:26 -0500

    min 5 characteres in account alias

--- src/accounts/helpers/validations.js
+++ src/accounts/helpers/validations.js
@@ -1,25 +1,25 @@
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 export const addAccountValidation = Yup.object().shape({
   account_number: Yup.string()
-    .required('Debes ingresar tu número de cuenta.')
-    .matches(/^[0-9]{13,14}$/, 'Número de cuenta inválido. Verifica los datos.'),
-  bankId: Yup.number().required('Debes seleccionar un banco.'),
-  currencyId: Yup.string().required('Debes seleccionar una moneda.'),
-  alias: Yup.string().required('Debes ingresar un alias.').max(40, 'No deben ser más de 40 caracteres.'),
-  acc_type: Yup.string().required('Debes seleccionar un tipo de cuenta.'),
-  accept: Yup.boolean().oneOf([true], 'Debes declarar que es tu cuenta personal.'),
+    .required("Debes ingresar tu número de cuenta.")
+    .matches(/^[0-9]{13,14}$/, "Número de cuenta inválido. Verifica los datos."),
+  bankId: Yup.number().required("Debes seleccionar un banco."),
+  currencyId: Yup.string().required("Debes seleccionar una moneda."),
+  alias: Yup.string().required("Debes ingresar un alias.").min(5, "Debe ser mínimo de 5 caracteres.").max(40, "No deben ser más de 40 caracteres."),
+  acc_type: Yup.string().required("Debes seleccionar un tipo de cuenta."),
+  accept: Yup.boolean().oneOf([true], "Debes declarar que es tu cuenta personal."),
 });
 
 export const editAccountValidation = Yup.object().shape({
   account_number: Yup.string()
-    .required('Debes ingresar tu número de cuenta.')
-    .matches(/^[0-9]{13,14}$/, 'Número de cuenta inválido. Verifica los datos.'),
-  alias: Yup.string().required('Debes ingresar un alias.').max(40, 'No deben ser más de 40 caracteres.'),
+    .required("Debes ingresar tu número de cuenta.")
+    .matches(/^[0-9]{13,14}$/, "Número de cuenta inválido. Verifica los datos."),
+  alias: Yup.string().required("Debes ingresar un alias.").max(40, "No deben ser más de 40 caracteres."),
 });
 
 export const kashWithdrawalValidation = (amount) =>
   Yup.object().shape({
-    kashQty: Yup.number().required('Debes colocar la cantidad a retirar').max(amount, `No puedes retirar más de la cantidad que posees. (${amount} kash).`),
-    accountId: Yup.number().required('Debes seleccionar la cuenta donde recibirás.'),
+    kashQty: Yup.number().required("Debes colocar la cantidad a retirar").max(amount, `No puedes retirar más de la cantidad que posees. (${amount} kash).`),
+    accountId: Yup.number().required("Debes seleccionar la cuenta donde recibirás."),
   });
