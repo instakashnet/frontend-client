793b029ff36ab728d3a6adc10ca8a416428888f1
Roger Rengifo <thelea12@gmail.com>
2021-07-23 11:14:31 -0500

    adding interbank modal and code change in 4001

--- src/profile/containers/selection.screen.js
+++ src/profile/containers/selection.screen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectProfileInit, openModal, closeModal } from "../../store/actions";
 import { AlertTriangle } from "react-feather";
@@ -17,17 +17,17 @@ const Selection = () => {
 
   const addProfileHandler = () => dispatch(openModal(AddProfile));
 
-  // useEffect(() => {
-  //   let timeout;
-  //   const isRead = sessionStorage.getItem("isRead");
+  useEffect(() => {
+    let timeout;
+    const isRead = sessionStorage.getItem("isRead");
 
-  //   if (!isRead) {
-  //     timeout = setTimeout(() => {
-  //       dispatch(openModal(InfoModal));
-  //     }, 600);
-  //   }
-  //   return () => timeout && clearTimeout(timeout);
-  // }, [dispatch]);
+    if (!isRead) {
+      timeout = setTimeout(() => {
+        dispatch(openModal(InfoModal));
+      }, 600);
+    }
+    return () => timeout && clearTimeout(timeout);
+  }, [dispatch]);
 
   return (
     <Layout>
@@ -67,8 +67,8 @@ export const InfoModal = () => {
       <AlertTriangle size={70} className="error-msg mb-4" />
       <h2>Estimado usuario</h2>
       <p>
-        Gracias por la confianza, queremos informarle que desde hace días la plataforma para empresas de <b>Interbank</b> posee problemas en su plataforma, por tal motivo no
-        podremos realizar solicitues hacia este banco por el momento.
+        Gracias por la confianza, queremos informarle que dla plataforma para empresas de <b>Interbank</b> posee problemas en su plataforma, por tal motivo las operaciones hacia
+        este banco podrán demorar más de lo usual.
         <span className="mt-4 inline-block font-bold">Agradecemos su comprensión.</span>
         <Button onClick={closeModalHandler} className="action-button">
           Lo entiendo
