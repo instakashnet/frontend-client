ec355d8c2811172128c4dc3cbace02e2c4a2c2af
Roger Rengifo <thelea12@gmail.com>
2021-08-25 16:38:43 -0500

    modal close ibk

--- src/services/accounts.service.js
+++ src/services/accounts.service.js
@@ -3,7 +3,7 @@ import { reqInterceptor, resInterceptor } from "../shared/axios/interceptors";
 
 export const accountsService = axios.create({
   baseURL: `${process.env.NODE_ENV !== "production" ? process.env.REACT_APP_TEST_ACCOUNTS_API : process.env.REACT_APP_ACCOUNTS_API}/client`,
-  timeout: 15000,
+  timeout: 25000,
 });
 
 reqInterceptor(accountsService);
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,7 +3,7 @@ import { reqInterceptor, resInterceptor } from "../shared/axios/interceptors";
 
 export const authService = axios.create({
   baseURL: `${process.env.NODE_ENV !== "production" ? process.env.REACT_APP_TEST_AUTH_API : process.env.REACT_APP_AUTH_API}/client`,
-  timeout: 15000,
+  timeout: 25000,
 });
 
 reqInterceptor(authService);
