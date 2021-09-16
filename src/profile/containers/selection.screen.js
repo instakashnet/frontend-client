f5adf7d01aaa1b0cf62c3c177d739fe8f2a81f62
Roger Rengifo <thelea12@gmail.com>
2021-09-16 14:32:46 -0500

    BCP modal

--- src/profile/containers/selection.screen.js
+++ src/profile/containers/selection.screen.js
@@ -69,8 +69,8 @@ export const ModalInformation = () => {
     <div className="text-center">
       <h2>Estimado usuario</h2>
       <p className="my-3">
-        Gracias por la confianza, queremos informarle que en estos momentos la plataforma para empresas de <b>BCP</b> presenta problemas en su plataforma. Por tal movito las
-        operaciones a <b>BCP</b> pueden demorar más de la usual. Estamos a la espera del banco que solucione a la brevedad posible.
+        Gracias por la confianza, queremos informarle que en estos momentos la plataforma para empresas de <b>BCP</b> presenta problemas en su plataforma. Por tal movito no
+        realizaremos operaciones a este banco por el momento. Estamos a la espera del banco que solucione a la brevedad posible.
       </p>
       <p className="mb-4 inline-block font-bold">Agradecemos su comprensión.</p>
       <Button onClick={closeModalHandler} className="action-button">
