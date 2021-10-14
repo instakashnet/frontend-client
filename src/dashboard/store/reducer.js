09729a50ff14aa724cc0cfc3cc37305d64c18cd1
Roger Rengifo <thelea12@gmail.com>
2021-10-14 08:42:04 -0500

    dashboard loading state

--- src/dashboard/store/reducer.js
+++ src/dashboard/store/reducer.js
@@ -11,18 +11,17 @@ const initialState = {
 export default function activityReducer(state = initialState, action) {
   switch (action.type) {
     case types.GET_ORDERS_INIT:
-    case types.GET_WITHDRAWALS_INIT:
       return { ...state, isLoading: true };
     case types.GET_ORDERS_SUCCESS:
       const filteredOrders = action.orders.reverse();
       return { ...state, orders: filteredOrders, isLoading: false };
     case types.GET_WITHDRAWALS_SUCCESS:
       const filteredWithdrawals = action.withdrawals.reverse();
-      return { ...state, withdrawals: filteredWithdrawals, isLoading: false };
+      return { ...state, withdrawals: filteredWithdrawals };
     case types.GET_ORDER_AMOUNTS_SUCCESS:
-      return { ...state, orderAmounts: action.orderAmounts, isLoading: false };
+      return { ...state, orderAmounts: action.orderAmounts };
     case types.GET_TOTAL_AMOUNT_SUCCESS:
-      return { ...state, totalAmount: action.totalAmount, isLoading: false };
+      return { ...state, totalAmount: action.totalAmount };
     case types.GET_ORDER_DETAILS_SUCCESS:
       return { ...state, details: action.details };
     case types.ACTIVITY_ERROR:
