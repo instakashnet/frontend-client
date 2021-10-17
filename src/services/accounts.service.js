564b91518fe010cbbd5450d20de0316d87d7409d
Roger Rengifo <rogerrengifo@MacBook-Pro-de-Roger.local>
2021-10-17 12:35:17 -0500

    Cloudfront

--- src/services/accounts.service.js
+++ src/services/accounts.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { reqInterceptor, resInterceptor } from "../shared/axios/interceptors";
 
 export const accountsService = axios.create({
-  baseURL: process.env.NODE_ENV !== "production" ? process.env.REACT_APP_TEST_ACCOUNTS_API : process.env.REACT_APP_ACCOUNTS_API
+  baseURL: `${process.env.NODE_ENV !== "production" ? process.env.REACT_APP_TEST_ACCOUNTS_API : process.env.REACT_APP_ACCOUNTS_API}/client`,
 });
 
 reqInterceptor(accountsService);
--- src/services/exchange.service.js
+++ src/services/exchange.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { reqInterceptor, resInterceptor } from "../shared/axios/interceptors";
 
 export const exchangeService = axios.create({
-  baseURL: process.env.NODE_ENV !== "production" ? process.env.REACT_APP_TEST_EXCHANGE_API : process.env.REACT_APP_EXCHANGE_API,
+  baseURL: `${process.env.NODE_ENV !== "production" ? process.env.REACT_APP_TEST_EXCHANGE_API : process.env.REACT_APP_EXCHANGE_API}/client`,
 });
 
 reqInterceptor(exchangeService);
