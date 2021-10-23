b3faa12eb6b087433897f9e107230ec9e506757e
Roger Rengifo <rogerrengifo@MacBook-Pro-de-Roger.local>
2021-10-23 11:14:26 -0500

    modal

--- src/profile/containers/selection.screen.js
+++ src/profile/containers/selection.screen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectProfileInit, openModal, closeModal } from "../../store/actions";
 
@@ -20,17 +20,17 @@ const Selection = () => {
     ModalComponent = () => <AddProfile title="Agregar empresa" />;
     dispatch(openModal(ModalComponent));
   };
-  // useEffect(() => {
-  //   let timeout;
-  //   const isRead = sessionStorage.getItem("isRead");
+  useEffect(() => {
+    let timeout;
+    const isRead = sessionStorage.getItem("isRead");
 
-  //   if (!isRead) {
-  //     timeout = setTimeout(() => {
-  //       dispatch(openModal(ModalComponent));
-  //     }, 600);
-  //   }
-  //   return () => timeout && clearTimeout(timeout);
-  // }, [dispatch]);
+    if (!isRead) {
+      timeout = setTimeout(() => {
+        dispatch(openModal(ModalComponent));
+      }, 600);
+    }
+    return () => timeout && clearTimeout(timeout);
+  }, [dispatch]);
 
   return (
     <Layout>
@@ -69,8 +69,8 @@ export const ModalInformation = () => {
     <div className="text-center">
       <h2>Estimado usuario</h2>
       <p className="my-3">
-        Gracias por la confianza, queremos informarle que en estos momentos presentamos una demora en todas nuestras operaciones debido a un gran flujo de pedidos. Estamos
-        trabajando para procesarlas a la brevedad posible.
+        Gracias por la confianza, queremos informarle que hoy Sábado 23 de octubre estaremos haciendo mantenimiento a nuestros servidores a las 2PM hora Perú, para poder brindarle
+        siempre el mejor servicio. Toda operación ingresada <b>despúes de las 2PM de hoy</b> será procesada el dia Lunes a las 9AM.
       </p>
       <p className="mb-4 inline-block font-bold">Agradecemos su comprensión.</p>
       <Button onClick={closeModalHandler} className="action-button">
