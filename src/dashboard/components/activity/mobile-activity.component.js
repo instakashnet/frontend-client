4dfaa92c4f5738eb274bc61f1f2831037c5a34f2
Roger Rengifo <rogerrengifo@MacBook-Pro-de-Roger.local>
2021-10-27 10:27:59 -0500

    mobile view on dashboard items

--- src/dashboard/components/activity/mobile-activity.component.js
+++ src/dashboard/components/activity/mobile-activity.component.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import _ from "lodash";
 import SwipeableViews from "react-swipeable-views";
 import { Repeat } from "@material-ui/icons";
+import { Pagination } from "@material-ui/lab";
 
 import Card from "../../../components/UI/card.component";
 import { OrderItem } from "../orders/order-item.component";
@@ -9,16 +10,22 @@ import { OrderItem } from "../orders/order-item.component";
 import classes from "../../assets/css/activity-components.module.scss";
 
 export const ActivityMobile = ({ orders, withdrawals, openModal }) => {
-  const groupedOrders = _.chunk(orders, 4);
-  const groupedWithdrawals = _.chunk(withdrawals, 4);
+  const [ordersPage, setOrdersPage] = useState(1),
+    [withdrawalsPage, setWithdrawalsPage] = useState(1),
+    groupedOrders = _.chunk(orders, 4),
+    groupedWithdrawals = _.chunk(withdrawals, 4);
+
+  // HANDLERS
+  const onOrdersChange = (index) => setOrdersPage(index + 1),
+    onWithdrawalsChange = (index) => setWithdrawalsPage(index + 1);
 
   return (
-    <div>
+    <>
       <div className={classes.DashboardCard}>
         <h2 className="flex font-bold items-center mb-2">
           <Repeat className="mr-2" /> Últimos cambios de divisa
         </h2>
-        <SwipeableViews>
+        <SwipeableViews style={{ padding: "0 10px 0 0" }} index={ordersPage - 1} slideStyle={{ padding: "0 5px" }} onChangeIndex={onOrdersChange}>
           {groupedOrders.map((orders, i) => (
             <div className="flex flex-col items-center py-4" key={i}>
               {orders.map((order) => (
@@ -29,12 +36,25 @@ export const ActivityMobile = ({ orders, withdrawals, openModal }) => {
             </div>
           ))}
         </SwipeableViews>
+        {groupedOrders.length > 1 && <Pagination count={groupedOrders.length} page={ordersPage} onChange={(_, value) => setOrdersPage(value)} color="primary" />}
       </div>
       <div className={classes.DashboardCard}>
-        <h2 className="flex items-center mb-2 mt-6">
+        <h2 className="flex items-center mb-2">
           <Repeat className="mr-2" /> Últimos retiros KASH
         </h2>
+        <SwipeableViews style={{ padding: "0 10px 0 0" }} index={withdrawalsPage - 1} slideStyle={{ padding: "0 5px" }} onChangeIndex={onWithdrawalsChange}>
+          {groupedWithdrawals.map((withdrawals, i) => (
+            <div className="flex flex-col items-center py-4" key={i}>
+              {withdrawals.map((widthdrawal) => (
+                <Card key={widthdrawal.id} className="mb-5 w-full">
+                  <OrderItem order={widthdrawal} type="widthdrawal" openModal={openModal} isMobile />
+                </Card>
+              ))}
+            </div>
+          ))}
+        </SwipeableViews>
+        {withdrawalsPage.length > 1 && <Pagination count={groupedWithdrawals.length} page={withdrawalsPage} onChange={(_, value) => setWithdrawalsPage(value)} color="primary" />}
       </div>
-    </div>
+    </>
   );
 };
--- src/dashboard/containers/dashboard.screen.js
+++ src/dashboard/containers/dashboard.screen.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
 import { Route } from "react-router-dom";
+import { useDeviceDetect } from "../../shared/hooks/useDeviceDetect";
+
+// REDUX
+import { useSelector, useDispatch } from "react-redux";
 import { openSliderModal, getOrderDetailsInit, getAccountsInit, getOrdersInit, getWithdrawalsInit } from "../../store/actions";
 
 import Layout from "../../components/layout/layout.component";
@@ -18,7 +21,19 @@ const Dashboard = ({ match }) => {
   const dispatch = useDispatch();
   const { orders, withdrawals, orderAmounts, totalAmount, isLoading } = useSelector((state) => state.Dashboard);
   const { kashAccount } = useSelector((state) => state.Accounts);
+  const { isMobile } = useDeviceDetect();
 
+  // EFFECTS
+  useEffect(() => {
+    dispatch(getAccountsInit("kash"));
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(getOrdersInit());
+    dispatch(getWithdrawalsInit());
+  }, [dispatch]);
+
+  // HANDLERS
   const openDetails = (id, type = null) => {
     let SliderComponent;
 
@@ -28,15 +43,6 @@ const Dashboard = ({ match }) => {
     dispatch(getOrderDetailsInit(id, type));
     dispatch(openSliderModal(SliderComponent));
   };
-
-  useEffect(() => {
-    dispatch(getAccountsInit("kash"));
-  }, [dispatch]);
-
-  useEffect(() => {
-    dispatch(getOrdersInit());
-    dispatch(getWithdrawalsInit());
-  }, [dispatch]);
 
   return (
     <Layout className={`${orders.length <= 0 && withdrawals.length <= 0 ? "content-center" : "content-start"} max-screen`}>
@@ -49,8 +55,12 @@ const Dashboard = ({ match }) => {
           <Route exact path={match.url}>
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
               <TransferedCharts isLoading={isLoading} orderAmounts={orderAmounts} />
-              <ActivityMobile openModal={openDetails} withdrawals={withdrawals} orders={orders} />
-              {/* <RecentActivity openModal={openDetails} withdrawals={withdrawals} isLoading={isLoading} orders={orders} /> */}
+              {isMobile ? (
+                <ActivityMobile openModal={openDetails} withdrawals={withdrawals} orders={orders} />
+              ) : (
+                <RecentActivity openModal={openDetails} withdrawals={withdrawals} isLoading={isLoading} orders={orders} />
+              )}
+
               <TotalTransfered kashAccount={kashAccount} totalAmount={totalAmount} isLoading={isLoading} className="flex-col" />
             </div>
           </Route>
