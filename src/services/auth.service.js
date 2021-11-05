db6724c4037117438042cc3d739442279a5a0f95
Roger Rengifo <rogerrengifo@MacBook-Pro-de-Roger.local>
2021-11-04 22:12:35 -0500

    new endpoint prod

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { reqInterceptor, resInterceptor } from "../shared/axios/interceptors";
 
 export const authService = axios.create({
-  baseURL: process.env.REACT_APP_STAGE !== "prod" ? `${process.env.REACT_APP_DEV_API_URL}/auth-service/api/v1/client` : `${process.env.REACT_APP_AUTH_API}/client`,
+  baseURL: `${process.env.REACT_APP_STAGE === "prod" ? process.env.REACT_APP_API_URL : process.env.REACT_APP_DEV_API_URL}/auth-service/api/v1/client`,
 });
 
-reqInterceptor(authService, "auth");
+reqInterceptor(authService);
 resInterceptor(authService);
--- src/services/exchange.service.js
+++ src/services/exchange.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { reqInterceptor, resInterceptor } from "../shared/axios/interceptors";
 
 export const exchangeService = axios.create({
-  baseURL: process.env.REACT_APP_STAGE !== "prod" ? `${process.env.REACT_APP_DEV_API_URL}/exchange-service/api/v1/client` : `${process.env.REACT_APP_EXCHANGE_API}/client`,
+  baseURL: `${process.env.REACT_APP_STAGE === "prod" ? process.env.REACT_APP_API_URL : process.env.REACT_APP_DEV_API_URL}/exchange-service/api/v1/client`,
 });
 
 reqInterceptor(exchangeService);
