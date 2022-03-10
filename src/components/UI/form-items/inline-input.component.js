bf0528823308f1d5e9d99185b8767298bef29fc9
Javier Valeriano <javier.valeriano@instakash.net>
2022-03-10 12:42:16 -0400

    Implementing MUI's FormControls: Second commit

--- src/components/UI/form-items/inline-input.component.js
+++ src/components/UI/form-items/inline-input.component.js
@@ -1,16 +1,26 @@
 import React from "react";
-import { TextField, OutlinedInput, InputLabel, FormControl } from "@material-ui/core";
+import { TextField, OutlinedInput, InputLabel, FormHelperText, FormControl, makeStyles } from "@material-ui/core";
 
 import { Button } from "../button.component";
 
 import classes from "../modules/form/inline_upload-input.module.scss";
 
+// const styles = makeStyles({
+//   inlineInput: {
+//     "& legend": {
+//       visibility: "visible",
+//       minWidth: "max-content"
+//     }
+//   }
+// });
+
 export const InlineInput = ({ type, name, value, disabled, error, touched, buttonLabel, className, buttonType, onClick, helperText, label, ...rest }) => {
+  // const useStyle = styles();
   return (
     // <div className={`${classes.FormGroup} ${className || ""}`}>
     <FormControl classes={{ root: classes.FormGroup }}>
       <div className={classes.FlexInput}>
-        {/* <TextField
+        <TextField
           type={type}
           name={name}
           error={!!error && !!touched}
@@ -20,19 +30,19 @@ export const InlineInput = ({ type, name, value, disabled, error, touched, butto
           helperText={error && touched ? error : helperText ? helperText : ""}
           classes={{ root: classes.FormControl }}
           {...rest}
-        /> */}
-        <InputLabel htmlFor="inline-input">{label}</InputLabel>
+        />
+        {/* <InputLabel>{label}</InputLabel>
         <OutlinedInput
           id="inline-input"
           aria-describedby=""
-          variant="outlined"
           type={type}
           name={name}
+          label={label}
           error={!!error && !!touched}
           value={value}
           classes={{ root: classes.FormControl }}
           {...rest}
-        />
+        /> */}
         {buttonType === "submit" ? (
           <Button type="submit" disabled={disabled}>
             {buttonLabel}
@@ -45,6 +55,7 @@ export const InlineInput = ({ type, name, value, disabled, error, touched, butto
       </div>
 
       {error && touched && <p className="error-msg">{error}</p>}
+        {/* <FormHelperText></FormHelperText> */}
       </FormControl>
     // </div>
   );
