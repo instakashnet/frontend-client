ab3ff5ffd7e935ccc0b9a6016f973f83ad0f4bf7
Javier Valeriano <javier.valeriano@instakash.net>
2022-03-21 22:53:15 -0400

    SCSS code, Error500, and Error404 screens edited

--- src/pages/errors/containers/error-404.screen.js
+++ src/pages/errors/containers/error-404.screen.js
@@ -1,19 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// ASSETS && CLASSES
-import CloudsSKash404 from "../assets/images/404_vector.svg";
-import SuperKash404 from "../assets/images/superkash_404.svg";
+// CLASSES
 import classes from "./modules/error-404.module.scss";
 
 const Error404 = () => {
-  const mobile = window.innerWidth < 768;
-
   return (
     <section className={classes.Error404Wrapper}>
-      <div className={classes.SuperKashWrapper}>
-        <img src={mobile ? SuperKash404 : CloudsSKash404} alt="SuperKash" />
-      </div>
+      <div className={classes.SuperKash404} />
       <div className={classes.Error404Desc}>
         <h1>404</h1>
         <h2>No encontramos la página.</h2>
@@ -21,7 +15,7 @@ const Error404 = () => {
           Tal vez copiaste mal la dirección. <span className="font-bold">¡Hasta allá no llega SuperKash!</span><br/>
           Por favor, regresa.
         </p>
-        <Link to="/">Inicio</Link>
+        <Link to="/">Regresar</Link>
       </div>
     </section>
   );
