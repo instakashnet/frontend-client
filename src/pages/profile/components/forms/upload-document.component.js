42c0bd7b9f4ec51074dd3c53cdef2be7250bc334
Javier Valeriano <jvalerianoz15@gmail.com>
2022-05-21 11:07:44 -0400

    Unnecesary React.fragment deleted

--- src/pages/profile/components/forms/upload-document.component.js
+++ src/pages/profile/components/forms/upload-document.component.js
@@ -58,64 +58,62 @@ export const UploadDocument = ({ docType }) => {
 
   return (
     <div className={classes.UploadDocWrapper}>
-      <>
-        <h2>Toma una foto</h2>
-        <p>Sube una foto de tu documento siguiendo las indicaciones para evitar que pueda haber rechazos en la verificación de tu identidad.</p>
-        <div className="flex items-center justify-center my-3">
-          <img src={CorrectDoc} width={125} alt="documento correcto" className="mx-2" />
-          <img src={IncorrectDoc} width={100} alt="documento incorrecto" className="mx-2" />
-        </div>
-        <h3>¡Importante!</h3>
-        <ul className="my-2">
-          <li>La fotografía no debe estar borrosa, desenfocada ni pixelada.</li>
-          <li>Verifique que la fotografía sea cercana y que no hayan otros elementos en la imagen.</li>
-          <li>Toda la información que aparece en el documento debe ser totalmente legible.</li>
-          <li>El tipo y número de documento capturado debe ser el mismo usado en tu registro.</li>
-          <li>La imagen no debe pesar más de 10MB.</li>
-        </ul>
-        {docType === "passport" ? (
-          <label htmlFor="passport-photo" className={classes.PassportButton}>
-            <input type="file" name="passport-photo" id="passport-photo" accept=".jpg, .jpeg" capture="environment" onChange={(e) => onTakePhoto(e, "front")} />
+      <h2>Toma una foto</h2>
+      <p>Sube una foto de tu documento siguiendo las indicaciones para evitar que pueda haber rechazos en la verificación de tu identidad.</p>
+      <div className="flex items-center justify-center my-3">
+        <img src={CorrectDoc} width={125} alt="documento correcto" className="mx-2" />
+        <img src={IncorrectDoc} width={100} alt="documento incorrecto" className="mx-2" />
+      </div>
+      <h3>¡Importante!</h3>
+      <ul className="my-2">
+        <li>La fotografía no debe estar borrosa, desenfocada ni pixelada.</li>
+        <li>Verifique que la fotografía sea cercana y que no hayan otros elementos en la imagen.</li>
+        <li>Toda la información que aparece en el documento debe ser totalmente legible.</li>
+        <li>El tipo y número de documento capturado debe ser el mismo usado en tu registro.</li>
+        <li>La imagen no debe pesar más de 10MB.</li>
+      </ul>
+      {docType === "passport" ? (
+        <label htmlFor="passport-photo" className={classes.PassportButton}>
+          <input type="file" name="passport-photo" id="passport-photo" accept=".jpg, .jpeg" capture="environment" onChange={(e) => onTakePhoto(e, "front")} />
+          {frontPhoto ? (
+            <img src={frontPhoto} alt="foto pasaporte" />
+          ) : (
+            <>
+              <CameraAlt fontSize="small" htmlColor="#20a2a5" />
+              <p>Tomar foto pasaporte</p>
+            </>
+          )}
+        </label>
+      ) : (
+        <div className="flex items-center">
+          <label htmlFor="front-photo" className={classes.PhotoButtonSquare}>
+            <input type="file" name="front-photo" id="front-photo" accept=".jpg, .jpeg" capture="environment" onChange={(e) => onTakePhoto(e, "front")} />
             {frontPhoto ? (
-              <img src={frontPhoto} alt="foto pasaporte" />
+              <img src={frontPhoto} alt="foto frontal" />
             ) : (
               <>
                 <CameraAlt fontSize="small" htmlColor="#20a2a5" />
-                <p>Tomar foto pasaporte</p>
+                <p>Tomar foto frontal</p>
               </>
             )}
           </label>
-        ) : (
-          <div className="flex items-center">
-            <label htmlFor="front-photo" className={classes.PhotoButtonSquare}>
-              <input type="file" name="front-photo" id="front-photo" accept=".jpg, .jpeg" capture="environment" onChange={(e) => onTakePhoto(e, "front")} />
-              {frontPhoto ? (
-                <img src={frontPhoto} alt="foto frontal" />
-              ) : (
-                <>
-                  <CameraAlt fontSize="small" htmlColor="#20a2a5" />
-                  <p>Tomar foto frontal</p>
-                </>
-              )}
-            </label>
-            <label htmlFor="back-photo" className={classes.PhotoButtonSquare}>
-              <input type="file" name="back-photo" id="back-photo" accept=".jpg, .jpeg" capture="environment" onChange={(e) => onTakePhoto(e, "back")} />
-              {backPhoto ? (
-                <img src={backPhoto} alt="foto reversa" />
-              ) : (
-                <>
-                  <CameraAlt fontSize="small" htmlColor="#20a2a5" />
-                  <p>Tomar foto reverso</p>
-                </>
-              )}
-            </label>
-          </div>
-        )}
-        <Button className={`action-button mt-6 ld-over ${isProcessing ? "running" : ""}`} onClick={uploadDocument} disabled={(docType === "dni" && !backPhoto) || !frontPhoto}>
-          <span className="ld ld-ring ld-spin" />
-          Subir documento
-        </Button>
-      </>
+          <label htmlFor="back-photo" className={classes.PhotoButtonSquare}>
+            <input type="file" name="back-photo" id="back-photo" accept=".jpg, .jpeg" capture="environment" onChange={(e) => onTakePhoto(e, "back")} />
+            {backPhoto ? (
+              <img src={backPhoto} alt="foto reversa" />
+            ) : (
+              <>
+                <CameraAlt fontSize="small" htmlColor="#20a2a5" />
+                <p>Tomar foto reverso</p>
+              </>
+            )}
+          </label>
+        </div>
+      )}
+      <Button className={`action-button mt-6 ld-over ${isProcessing ? "running" : ""}`} onClick={uploadDocument} disabled={(docType === "dni" && !backPhoto) || !frontPhoto}>
+        <span className="ld ld-ring ld-spin" />
+        Subir documento
+      </Button>
     </div>
   );
 };
