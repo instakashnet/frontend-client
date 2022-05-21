f977d9f49b9b932c35e1a8f672cbe6942e903876
Javier Valeriano <jvalerianoz15@gmail.com>
2022-05-21 10:36:02 -0400

    React-webcam replacement by input file completed

--- src/pages/profile/components/forms/upload-document.component.js
+++ src/pages/profile/components/forms/upload-document.component.js
@@ -7,21 +7,20 @@ import CorrectDoc from "../../../../assets/images/illustrations/correct-doc.svg"
 import IncorrectDoc from "../../../../assets/images/illustrations/incorrect-doc.svg";
 // COMPONENT
 import { Button } from "../../../../components/UI/button.component";
+// HELPER
+import { encodeFileToBase64URL } from "../../../../shared/functions";
 // REDUX ACTION
 import { uploadDocumentInit } from "../../../../store/actions";
 // CLASSES
 import classes from "../modules/forms/upload-document.module.scss";
-// COMPONENT
-// import { DocumentCamera } from "./document-camera.component";
-
 
 export const UploadDocument = ({ docType }) => {
   const dispatch = useDispatch(),
     isProcessing = useSelector((state) => state.Profile.isProcessing),
-    [openCamera, setOpenCamera] = useState(false),
-    [photoSide, setPhotoSide] = useState(""),
     [frontPhoto, setFrontPhoto] = useState(null),
-    [backPhoto, setBackPhoto] = useState(null);
+    [backPhoto, setBackPhoto] = useState(null),
+    [frontPhotoBase64, setFrontPhotoBase64] = useState(null),
+    [backPhotoBase64, setBackPhotoBase64] = useState(null);
 
   // HANDLERS
   const onTakePhoto = async (e, side) => {
@@ -37,25 +36,25 @@ export const UploadDocument = ({ docType }) => {
       if (fileTaken.length <= 0) return;
       if (fileTaken[0].size >= 10485760) return alert("La imagen no debe pesar más de 10MB.");
 
-      const URLFileTaken = URL.createObjectURL(fileTaken[0]);
+      const URLFilePreview = (window.URL || window.webkitURL).createObjectURL(fileTaken[0]),
+        dataImgBase64 = await encodeFileToBase64URL(fileTaken[0]);
 
-      if (side === "front") setFrontPhoto(URLFileTaken);
-      if (side === "back") setBackPhoto(URLFileTaken);
+      if (side === "front") {
+        setFrontPhoto(URLFilePreview);
+        setFrontPhotoBase64(dataImgBase64);
+      };
+
+      if (side === "back") {
+        setBackPhoto(URLFilePreview);
+        setBackPhotoBase64(dataImgBase64);
+      };
     } catch (error) {
       console.log(error);
     }
   },
-    // setPhoto = useCallback(
-    //   (image) => {
-    //     if (photoSide === "front") setFrontPhoto(image);
-    //     if (photoSide === "back") setBackPhoto(image);
-    //     setOpenCamera(false);
-    //   },
-    //   [photoSide]
-    // ),
     uploadDocument = useCallback(() => {
-      dispatch(uploadDocumentInit({ front: frontPhoto, back: backPhoto }, docType));
-    }, [frontPhoto, backPhoto, docType, dispatch]);
+      dispatch(uploadDocumentInit({ front: frontPhotoBase64, back: backPhotoBase64 }, docType));
+    }, [frontPhotoBase64, backPhotoBase64, docType, dispatch]);
 
   return (
     <div className={classes.UploadDocWrapper}>
@@ -75,7 +74,8 @@ export const UploadDocument = ({ docType }) => {
           <li>La imagen no debe pesar más de 10MB.</li>
         </ul>
         {docType === "passport" ? (
-          <button onClick={() => onTakePhoto("front")} className={classes.PassportButton}>
+          <label htmlFor="passport-photo" className={classes.PassportButton}>
+            <input type="file" name="passport-photo" id="passport-photo" accept=".jpg, .jpeg, .png" capture="environment" onChange={(e) => onTakePhoto(e, "front")} />
             {frontPhoto ? (
               <img src={frontPhoto} alt="foto pasaporte" />
             ) : (
@@ -84,26 +84,26 @@ export const UploadDocument = ({ docType }) => {
                 <p>Tomar foto pasaporte</p>
               </>
             )}
-          </button>
+          </label>
         ) : (
           <div className="flex items-center">
             <label htmlFor="front-photo" className={classes.PhotoButtonSquare}>
+              <input type="file" name="front-photo" id="front-photo" accept=".jpg, .jpeg, .png" capture="environment" onChange={(e) => onTakePhoto(e, "front")} />
               {frontPhoto ? (
                 <img src={frontPhoto} alt="foto frontal" />
               ) : (
                 <>
-                  <input type="file" name="front-photo" id="front-photo" accept=".jpg, .jpeg, .png" capture="environment" onChange={(e) => onTakePhoto(e, "front")} />
                   <CameraAlt fontSize="small" htmlColor="#20a2a5" />
                   <p>Tomar foto frontal</p>
                 </>
               )}
             </label>
             <label htmlFor="back-photo" className={classes.PhotoButtonSquare}>
+              <input type="file" name="back-photo" id="back-photo" accept=".jpg, .jpeg, .png" capture="environment" onChange={(e) => onTakePhoto(e, "back")} />
               {backPhoto ? (
                 <img src={backPhoto} alt="foto reversa" />
               ) : (
                 <>
-                  <input type="file" name="back-photo" id="back-photo" accept=".jpg, .jpeg, .png" capture="environment" onChange={(e) => onTakePhoto(e, "back")} />
                   <CameraAlt fontSize="small" htmlColor="#20a2a5" />
                   <p>Tomar foto reverso</p>
                 </>
@@ -116,65 +116,6 @@ export const UploadDocument = ({ docType }) => {
           Subir documento
         </Button>
       </>
-      {/* {!openCamera ? (
-        <>
-          <h2>Toma una foto</h2>
-          <p>Sube una foto de tu documento siguiendo las indicaciones para evitar que pueda haber rechazos en la verificación de tu identidad.</p>
-          <div className="flex items-center justify-center my-3">
-            <img src={CorrectDoc} width={125} alt="documento correcto" className="mx-2" />
-            <img src={IncorrectDoc} width={100} alt="documento incorrecto" className="mx-2" />
-          </div>
-          <h3>¡Importante!</h3>
-          <ul className="my-2">
-            <li>La fotografía no debe estar borrosa, desenfocada ni pixelada.</li>
-            <li>Verifique que la fotografía sea cercana y que no hayan otros elementos en la imagen.</li>
-            <li>Toda la información que aparece en el documento debe ser totalmente legible.</li>
-            <li>El tipo y número de documento capturado debe ser el mismo usado en tu registro.</li>
-            <li>La imagen no debe pesar más de 10MB.</li>
-          </ul>
-          {docType === "passport" ? (
-            <button onClick={() => onTakePhoto("front")} className={classes.PassportButton}>
-              {frontPhoto ? (
-                <img src={frontPhoto} alt="foto pasaporte" />
-              ) : (
-                <>
-                  <CameraAlt fontSize="small" htmlColor="#20a2a5" />
-                  <p>Tomar foto pasaporte</p>
-                </>
-              )}
-            </button>
-          ) : (
-            <div className="flex items-center">
-              <button onClick={() => onTakePhoto("front")} className={classes.PhotoButtonSquare}>
-                {frontPhoto ? (
-                  <img src={frontPhoto} alt="foto frontal" />
-                ) : (
-                  <>
-                    <CameraAlt fontSize="small" htmlColor="#20a2a5" />
-                    <p>Tomar foto frontal</p>
-                  </>
-                )}
-              </button>
-              <button onClick={() => onTakePhoto("back")} className={classes.PhotoButtonSquare}>
-                {backPhoto ? (
-                  <img src={backPhoto} alt="foto reversa" />
-                ) : (
-                  <>
-                    <CameraAlt fontSize="small" htmlColor="#20a2a5" />
-                    <p>Tomar foto reverso</p>
-                  </>
-                )}
-              </button>
-            </div>
-          )}
-          <Button className={`action-button mt-6 ld-over ${isProcessing ? "running" : ""}`} onClick={uploadDocument} disabled={(docType === "dni" && !backPhoto) || !frontPhoto}>
-            <span className="ld ld-ring ld-spin" />
-            Subir documento
-          </Button>
-        </>
-      ) : (
-        <DocumentCamera setPhoto={setPhoto} />
-      )} */}
     </div>
   );
 };
